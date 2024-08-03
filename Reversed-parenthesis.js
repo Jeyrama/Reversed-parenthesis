8f8d896e8e2452c7ee3b9d698e28daa9af259417
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-08-03 13:35:13 -0700

    Update Reversed-parenthesis.js

--- Reversed-parenthesis.js
+++ Reversed-parenthesis.js
@@ -32,3 +32,18 @@ function solve(s) {
 }
 
 // or
+
+function solve(s) {
+  if (s.length % 2 != 0)
+    return -1;
+  let res = 0, k = 0;
+  for (let c of s) {
+    if (c == '(')
+      k++;
+    else if (k > 0)
+      k--;
+    else
+      k++, res++;
+  }
+  return res + k / 2;
+}
